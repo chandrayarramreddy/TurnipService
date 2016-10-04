48a5512ab4f6a3de8b131c27ce5368db3fb3ffc0
chandrayarramreddy <chandra.yarramreddy@gmail.com>
2016-10-05 01:59:30 +0530

    Update Constants.java

--- src/com/miracle/util/Constants.java
+++ src/com/miracle/util/Constants.java
@@ -4,11 +4,11 @@ public class Constants {
 	//172.17.16.151
 	//public static final String JDBC_URL = "jdbc:mysql://192.168.5.10:3306/sample";
    //  public static final String JDBC_URL = "jdbc:mysql://192.168.5.10:3306/sample";
-   public static final String JDBC_URL = "jdbc:mysql://172.17.13.50:3306/mysql";
+   public static final String JDBC_URL = "jdbc:mysql://192.168.5.10:3306/mysql";
 //	 public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "password";
+	public static final String PASSWORD = "ilikerandompasswords";
 
 	// Data base colomns
 	public static final String VENDOR_TIER_ID = "vendor_tier_id";
